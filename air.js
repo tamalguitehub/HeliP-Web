25172dd0eed7d04d4a92483114af0f0ddec62d15
Vishesh Sharma <imvishesharma@gmail.com>
2022-04-14 16:03:38 +0530

    Initial commit

---
+++ air.js
@@ -0,0 +1,31 @@
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyvcoBYjFEF0Z2mD'}).base('appY7E1lqNol7GdKF');
+
+base('ScoreData').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('PlayerName'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
+base('ScoreData').select({
+    view: 'Grid view'
+}).firstPage(function(err, records) {
+    if (err) { console.error(err); return; }
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('PlayerName'));
+    });
+});
