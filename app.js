0d13120ae716c8c1eb7d54be1870e3e790adcb79
Vishesh Sharma <imvishesharma@gmail.com>
2022-04-15 16:39:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,29 @@
 const WIDTH = 800;
 const HEIGHT = 600;
 
+let Enemies = [];
+let faceEast = [];
+
+fucntion tick() {
+    for(int i = 0; i < Enemies.length(); ++i) {
+        if(faceEast[i]) {
+            if(Enemies[i].x + 5 > WIDTH - 30) {
+                faceEast[i] = false;
+                Enemies[i].x -= 5;
+            } else {
+                Enemies[i].x +=  5;
+            }
+        }
+        else {
+            if(Enemies[i].x - 5 < 0) {
+                faceEast[i] = true;
+                Enemies[i].x += 5;
+            } else {
+                Enemies[i].x -=  5;
+            }
+        }
+    }
+}
 function myRandom(low, high) {
     return low + Math.floor(Math.random() * (high - low + 1));
 }
@@ -19,7 +42,12 @@ function GameObject(posX, posY, imgurl) {
 }
 
 function generateLevel() {
-    
+    const nEnemies = myRandom(5, 15);
+
+    for(let i = 0; i < nEnemies; ++i) {
+        Enemies.push(GameObject(myRandom(10, WIDTH - 50), myRandom(50, 150), 'Icons/enemy-helicopter256-fl.png'));
+        faceEast.push(true);
+    }
 }
 
 let app = new PIXI.Application({ width: WIDTH, height: HEIGHT, backgroundColor: 0xfae13c});
@@ -36,26 +64,9 @@ let player = GameObject(WIDTH / 2, HEIGHT - 50, 'Icons/player256-fl.png');
 app.stage.addChild(sprite);
 app.stage.addChild(player);
 
-let faceEast = true;
-
 // Add a ticker callback to move the sprite back and forth
 let elapsed = 0.0;
 app.ticker.add((delta) => {
   elapsed += delta;
-  if(faceEast) {
-      if(sprite.x + 5 > WIDTH - 30) {
-          faceEast = false;
-          sprite.x -= 5;
-      } else {
-          sprite.x +=  5;
-      }
-  }
-  else {
-      if(sprite.x - 5 < 0) {
-          faceEast = true;
-          sprite.x += 5;
-      } else {
-          sprite.x -=  5;
-      }
-  }
+  tick();
 });
