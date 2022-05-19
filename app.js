a5176070933f902814f87e105ff67184f2e70c36
Vishesh Sharma <imvishesharma@gmail.com>
2022-05-19 11:44:47 +0530

    .

--- app.js
+++ app.js
@@ -18,11 +18,19 @@ let enemyBuffImgW = PIXI.BaseTexture.from('Icons/enemy-helicopter256-fl.png');
 let enemyBuffImgE = PIXI.BaseTexture.from('Icons/enemy-helicopter256-fr.png');
 
 let bulletBuffImg = PIXI.BaseTexture.from('Icons/bullet256-fd.png');
+let medicBuffImg = PIXI.BaseTexture.from('Icons/pharmacy256.png');
+let barrierBuffImg = PIXI.BaseTexture.from('Icons/barrier256.png');
+
+const BULLET_DROP_PROB = 1500;
+const MEDIC_DROP_PROB = 100;
 
 let HealthX = 100;
 let Enemies = [];
 let faceEast = [];
 let Bullets = [];
+let Medics = [];
+let Barrier = [];
+let healthBarrier = [];
 
 let player = GameObject(WIDTH / 2, HEIGHT - 50, 0.2, 'Icons/player256-fl.png');
 
@@ -33,6 +41,10 @@ let tickCount = 0;
 const MAX_TICK_COUNT = 30;
 
 function tick() {
+    if(HealthX <= 0) {
+        isHelipRunning = false;
+    }
+
     tickCount++;
 
     let bulletGenerated = [];
@@ -59,12 +71,65 @@ function tick() {
         if(tickCount === MAX_TICK_COUNT) {
             var genBul = myRandom(0, 2000);
 
-            if(genBul < 1500) {
+            if(genBul < BULLET_DROP_PROB) {
                 var newBullet = GameObject(Enemies[i].x, Enemies[i].y, 0.1, bulletBuffImg);
                 bulletGenerated.push(newBullet);
                 console.log(newBullet.width + " " + newBullet.height);
                 app.stage.addChild(newBullet);
             }
+        }
+    }
+
+    var genMed = myRandom(0, 5000);
+
+    if(genMed < MEDIC_DROP_PROB) {
+        var newMedic = GameObject(myRandom(20, WIDTH - 20), myRandom(20, 50), 0.1, medicBuffImg);
+        Medics.push(newMedic);
+        //console.log(newBullet.width + " " + newBullet.height);
+        app.stage.addChild(newMedic);
+    }
+
+    for(let i = 0; i < Medics.length; ++i) {
+        Medics[i].y += 5;
+
+        var Xp = player.x;
+        var Yp = player.y;
+
+        var Xb = Medics[i].x;
+        var Yb = Medics[i].y;
+
+        var collided = false;
+        if(Xp > Xb && Xp - Xb < Medics[i].width) {
+            if(Yp > Yb && Yp - Yb < Medics[i].height) {
+                collided = true;
+            }
+            else if(Yp < Yb && Yb - Yp < player.height) {
+                collided = true;
+            }
+        }
+        else if(Xp < Xb && Xb - Xp < player.width) {
+            if(Yp > Yb && Yp - Yb < Medics[i].height) {
+                collided = true;
+            }
+            else if(Yp < Yb && Yb - Yp < player.height) {
+                collided = true;
+            }
+        }
+
+        if(collided) {
+            HealthX = Math.min(HealthX + 10, 100);
+            Medics[i].y = HEIGHT + 5;
+        }
+
+        if(Medics[i].y >= HEIGHT) {
+            if(i != 0) {
+                var tmp = Medics[0];
+                Medics[0] = Medics[i];
+                Medics[i] = tmp;
+            }
+
+            app.stage.removeChild(Medics[0]);
+            Medics.shift();
         }
     }
 
@@ -104,6 +169,43 @@ function tick() {
             if(collided) {
                 HealthX -= 5;
                 Bullets[i][j].y = HEIGHT + 5;
+            }
+
+            for(let k = 0; k < Barrier.length; ++k) {
+                var Xp = Barrier[k].x;
+                var Yp = Barrier[k].y;
+
+                var Xb = Bullets[i][j].x;
+                var Yb = Bullets[i][j].y;
+
+                var collided = false;
+
+                if(Xp > Xb && Xp - Xb < Bullets[i][j].width) {
+                    if(Yp > Yb && Yp - Yb < Bullets[i][j].height) {
+                        collided = true;
+                    }
+                    else if(Yp < Yb && Yb - Yp < Barrier.height) {
+                        collided = true;
+                    }
+                }
+                else if(Xp < Xb && Xb - Xp < Barrier[k].width) {
+                    if(Yp > Yb && Yp - Yb < Bullets[i][j].height) {
+                        collided = true;
+                    }
+                    else if(Yp < Yb && Yb - Yp < Barrier[k].height) {
+                        collided = true;
+                    }
+                }
+
+                if(collided) {
+                    healthBarrier[i] = Math.max(0, healthBarrier[i] - 1);
+                    Bullets[i][j].y = HEIGHT + 5;
+                }
+
+                if(healthBarrier[k] === 0) {
+                    healthBarrier[k] = -10;
+                    app.stage.removeChild(Barrier[k]);
+                }
             }
         }
     }
@@ -162,7 +264,7 @@ function GameObject(posX, posY, scale, imgBuff) {
 }
 
 function generateLevel() {
-    const nEnemies = myRandom(5, 15);
+    const nEnemies = 10; //myRandom(5, 15);
 
     console.log(nEnemies);
 
@@ -174,6 +276,14 @@ function generateLevel() {
             faceEast.push(false);
             Enemies.push(GameObject(myRandom(10, WIDTH - 50), myRandom(50, 150), 0.2, enemyBuffImgW));
         }
+    }
+
+    let nBarriers = myRandom(5, 10);
+    for(let i = 0; i < nBarriers; ++i) {
+        var newBarrier = GameObject(myRandom(10, WIDTH - 10), myRandom(HEIGHT - 200, HEIGHT - 100), 0.2, barrierBuffImg);
+        app.stage.addChild(newBarrier);
+        Barrier.push(newBarrier);
+        healthBarrier.push(5);
     }
 }
 
